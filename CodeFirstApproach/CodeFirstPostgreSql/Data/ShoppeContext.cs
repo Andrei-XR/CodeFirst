b9bca38be02b30020591f9b1f14a670a415d067b
Andrei Xavier <andrei_xavier@outlook.com>
2020-08-11 16:09:59 -0300

    add context

---
+++ CodeFirstApproach/CodeFirstPostgreSql/Data/ShoppeContext.cs
@@ -0,0 +1,19 @@
+﻿using CodeFirstPostgreSql.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFirstPostgreSql.Data
+{
+    public class ShoppeContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseNpgsql(Environment.GetEnvironmentVariable("StrConDb"));
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; } 
+    }
+}
