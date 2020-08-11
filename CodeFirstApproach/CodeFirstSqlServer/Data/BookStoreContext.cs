d60e55231ed34c73154771bc24b9f60240d91ba4
Andrei Xavier <andrei_xavier@outlook.com>
2020-08-11 12:39:27 -0300

    add context

---
+++ CodeFirstApproach/CodeFirstSqlServer/Data/BookStoreContext.cs
@@ -0,0 +1,20 @@
+﻿using CodeFirstSqlServer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFirstSqlServer.Data
+{
+    public class BookStoreContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Database=BookStore;Integrated Security=True;MultiSubnetFailover=False");
+        }
+    }
+}
