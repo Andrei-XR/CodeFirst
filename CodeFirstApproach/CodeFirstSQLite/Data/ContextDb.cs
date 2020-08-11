487ec26a156615509603931bea6f5db56f679411
Andrei Xavier <andrei_xavier@outlook.com>
2020-08-11 20:26:35 -0300

    add context

---
+++ CodeFirstApproach/CodeFirstSQLite/Data/ContextDb.cs
@@ -0,0 +1,19 @@
+﻿using CodeFirstSQLite.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFirstSQLite.Data
+{
+    public class ContextDb : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlite(@"Data Source=C:\SampleDb.db");
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
