64fbfaa8056009a184c582457477da09f7ca0d76
Andrei Xavier <andrei_xavier@outlook.com>
2020-08-11 12:39:56 -0300

    add models

---
+++ CodeFirstApproach/CodeFirstSqlServer/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFirstSqlServer.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
---
+++ CodeFirstApproach/CodeFirstSqlServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeFirstSqlServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
